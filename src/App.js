5751b016c82291b894033bbf974061f0af021634
Jvondamm <joshuavd@att.net>
2021-01-08 15:07:11 -0800

    finished step 6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form';
 
 class App extends Component {
     state = {
-      characters: [
-        {
-          name: 'Charlie',
-          job: 'Janitor',
-        },
-        {
-          name: 'Mac',
-          job: 'Bouncer',
-        },
-        {
-          name: 'Dee',
-          job: 'Aspring actress',
-        },
-        {
-          name: 'Dennis',
-          job: 'Bartender',
-        },
-      ]
+      characters: [],
     }
 
 
@@ -37,9 +21,15 @@ class App extends Component {
     render() {
       const { characters } = this.state
     
+      /*return (
+        <div className="container">
+          <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        </div>
+      )*/
       return (
         <div className="container">
           <Table characterData={characters} removeCharacter={this.removeCharacter} />
+          <Form />
         </div>
       )
     }
